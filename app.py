8ee160d888b5250a37a0c7297db0bcf5527fd5b9
Klymenko Illia <sovuh2703@gmail.com>
2023-11-27 14:23:49 +0200

    feat: add login and register template, and base forms

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 from flask import render_template, request, jsonify
+from flask_login import UserMixin
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
 from datetime import datetime
 
 
@@ -9,26 +14,78 @@ from utils.map_creator import MapCreator
 
 # create Flask app instance
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = '1234567891011123142adjhbakdhajsjdhabjhcbakjsajhcb'
 
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50))
+    username = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
     date_joined = db.Column(db.Date, default=datetime.utcnow)
 
     def __repr__(self):
-        return f'<User: {self.username}>'
+        return f"<User: {self.username}>"
+    
 
-    # User.query.all() - for getting all users
+class Map(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    html = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    date_created = db.Column(db.Date, default=datetime.utcnow)
+
+    user = relationship('User')
+
+    def __repr__(self):
+        return f"<Map: {self.id}>"
+    
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(
+            username=username.data).first()
+        if existing_user_username:
+            raise ValidationError(
+                'That username already exists. Please choose a different one.')
+        
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Login')
+
 
 @app.route("/")
 def index():
     # render index page
     return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    return render_template("login.html", form=form)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegisterForm()
+    return render_template("register.html", form=form)
 
 
 @app.route("/api/get_map/", methods=["POST"])
@@ -94,5 +151,3 @@ def get_headers():
     # get headers from excel file
     headers = get_headers_from_excel(file)
     return {"headers": headers}
-
-
